d990af55f6d08d42d73a93248531707dedeea99a
MikeinWang <Mikein@126.com>
2020-10-26 22:17:16 +0800

    First Commit

---
+++ LearnC/LearnC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LearnC
+//
+//  Created by 王镜淇 on 2020/10/26.
+//  Copyright © 2020 王镜淇. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
